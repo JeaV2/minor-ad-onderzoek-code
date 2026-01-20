068de7980ba24b84ece3383a19ae3533adf7288b
JeaV2 <102710@glr.nl>
2026-01-20 14:42:50 +0100

    Code for the program to exit do it doesn't end in a segfault

--- asm/helloworld.asm
+++ asm/helloworld.asm
@@ -11,4 +11,6 @@ mov ebx, 1 ; Schrijf naar stdout, de output dus.
 mov eax, 4 ; Systeemoproepnummer voor sys_write, om tekst te schrijven.
 int 80h
 
-
+mov ebx, 0 ; Exit code 0, wat betekent dat het programma succesvol is beëindigd.
+mov eax, 1 ; Systeemoproepnummer voor sys_exit, om het programma te beëindigen.
+int 80h
